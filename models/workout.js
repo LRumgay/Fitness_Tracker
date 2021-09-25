cfc0aad12fec165bee7f94272ecdcbd19e228158
Liam Rumgay <Liamrumgay2@gmail.com>
2021-09-25 18:59:16 -0500

    workout model finished

--- models/workout.js
+++ models/workout.js
@@ -1,18 +1,43 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const exerciseSchema = new Schema ({
-
+const exerciseSchema = new Schema({
+    
     day: {
         type: Date,
         default: Date.now
     },
-    exercises : [
+    exercises: [
         {
-            type: String,
-            trim: true,
-            required: ""
-        },
-    ]
+            type: {
+                type: String,
+                trime: true,
+                required: "Enter name for ..."
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter name for ..."
+            },
+            duration : {
+                type: Number
+            },
+            weight : {
+                type: Number
+            },
+            reps : {
+                type: Number
+            },
+            sets : {
+                type: Number
+            },
+            distance : {
+                type: Number
+            },
 
-})
+        }
+    ]
+});
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+module.exports = Exercise;
