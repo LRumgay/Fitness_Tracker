156eda054c7cd6c6677584119817c3c7a456c65c
Liam Rumgay <Liamrumgay2@gmail.com>
2021-09-26 20:38:51 -0500

    finished api routes, and index.js model

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,18 @@
 const router = require("express").Router();
+const { Workout } = require("../models");
 const workout = require("../models/workout");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        { $addfields: {totalDuration: {$sum: "$exercises.duration"}}}
+    ])
+    .then (workouts => {
+        res.json(workouts);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+});
 
 router.post("/api/workouts", (req, res) => {
     workout.create(req.body)
@@ -10,6 +23,22 @@ router.post("/api/workouts", (req, res) => {
         res.status(400).json(err);
     });
 });
+
+router.put("/api/workouts/:id"), (req, res) => {
+    Workout.findByIdAndUpdate(
+        { _id: req.params.id }, { $push: {exercises: req.body}}
+    )
+    .then(workout => {
+        res.json(workout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+};
+
+module.exports = router;
+
+
 
 
 
