afb2a6a04d98e4fc5441eafbccf32bafb880a704
Liam Rumgay <Liamrumgay2@gmail.com>
2021-10-01 23:44:48 -0500

    deploying

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require('morgan');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -12,13 +13,15 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+       useCreateIndex: true,
 });
 
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/stats.js"));
-// dont forget require for workout stats //
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
